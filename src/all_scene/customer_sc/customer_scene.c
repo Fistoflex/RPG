ab409d3e37d81a95d81fcf645b160419d1577158
Dimitri <dimitri-jean.clain@epitech.eu>
2019-04-02 13:23:20 +0400

    to_merge

--- src/all_scene/customer_sc/customer_scene.c
+++ src/all_scene/customer_sc/customer_scene.c
@@ -57,6 +57,10 @@ void draw_all_choice(sfRenderWindow *wind, list_t *list)
 
 list_t *make_your_choice(sfRenderWindow *wind, game_t *gm, list_t *list)
 {
+    if (list->iteme->str != NULL) {
+        printf("yes\n");
+        draw_all_choice(wind, list);
+    }
     if (gm->key.left == PUSHED) {
         list->iteme = list->iteme->next;
         gm->key.left = NOT_PUSHED;
@@ -73,7 +77,6 @@ list_t *make_your_choice(sfRenderWindow *wind, game_t *gm, list_t *list)
         list = list->next;
         gm->key.down = NOT_PUSHED;
     }
-    draw_all_choice(wind, list);
     return (list);
 }
 
@@ -83,14 +86,21 @@ list_t *init_my_custome(void)
     list_t *list = NULL;
     choice_t *hair = NULL;
     choice_t *top = NULL;
+    choice_t *body = NULL;
 
-    malloc_my_link_choice(&hair, my_create_sprite("picture/Character_assets/MALE/Male - Hair/black.png", sfFalse), "black");
-    malloc_my_link_choice(&hair, my_create_sprite("picture/Character_assets/MALE/Male - Hair/blue2.png", sfFalse), "blue");
-    malloc_my_link_choice(&hair, my_create_sprite("picture/Character_assets/MALE/Male - Hair/blonde2.png", sfFalse), "blond");
+
+    malloc_my_link_choice(&body, NULL, NULL);
+    malloc_my_link_choice(&body, my_create_sprite("picture/Character_assets/MALE/Male - Body/dark.png", sfFalse), "black");
+    malloc_my_link_choice(&body, my_create_sprite("picture/Character_assets/MALE/Male - Body/light.png", sfFalse), "black");
+    malloc_my_link_choice(&body, my_create_sprite("picture/Character_assets/MALE/Male - Body/tanned.png", sfFalse), "black");
+    malloc_my_link_choice(&hair, my_create_sprite("picture/Character_assets/MALE/Male - Hair/black-longhawk.png", sfFalse), "black");
+    malloc_my_link_choice(&hair, my_create_sprite("picture/Character_assets/MALE/Male - Hair/redhead2-bangs.png", sfFalse), "blue");
+    malloc_my_link_choice(&hair, my_create_sprite("picture/Character_assets/MALE/Male - Hair/blonde2-plain.png", sfFalse), "blond");
     malloc_my_link_choice(&top, my_create_sprite("picture/Character_assets/MALE/Male - Pants/magenta.png", sfFalse), "magenta_p");
     malloc_my_link_choice(&top, my_create_sprite("picture/Character_assets/MALE/Male - Pants/teal.png", sfFalse), "teal_p");
     malloc_my_link_choice(&top, my_create_sprite("picture/Character_assets/MALE/Male - Pants/red.png", sfFalse), "red_p");
     malloc_my_link_list(&list, hair);
+    malloc_my_link_list(&list, body);
     malloc_my_link_list(&list, top);
     return (list);
 }
